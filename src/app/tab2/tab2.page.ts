5369b2ba59c9979404fe663a52150fa088b7865a
guilhermealves1273 <guicardoso2002@hotmail.com>
2022-05-27 20:12:28 +0100

    continuação

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page  {
 
   constructor() {}
 
 }
+
+
 
 
