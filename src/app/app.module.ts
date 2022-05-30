7f62daf9158fa4f60c9b1ac9ea3fc3e249dc645e
guilhermealves1273 <guicardoso2002@hotmail.com>
2022-05-30 14:10:14 +0100

    hvty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
-
+import {Camera} from '@ionic-native/camera/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenOrientation],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenOrientation,Camera],
   bootstrap: [AppComponent],
 
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Camera} from '@ionic-native/camera/ngx';
 
 
 @Component({
@@ -8,9 +9,37 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page  {
 
-  constructor() {}
+imgURL;
+
+constructor(
+private camera:Camera
+  ) {
+}
+getCamera() {
+  this.camera.getPicture({
+    sourceType:this.camera.PictureSourceType.CAMERA,
+    destinationType:this.camera.DestinationType.FILE_URI,
+  }).then((res)  =>{
+
+this.imgURL = res;
+  }).catch(e=>{
+    console.log(e);
+  })
 
 }
+getGallery(){
+  this.camera.getPicture({
+    sourceType:this.camera.PictureSourceType.PHOTOLIBRARY,
+    destinationType:this.camera.DestinationType.DATA_URL
+  }).then((res)  =>{
+    this.imgURL = 'data:image/jpeg;base64,' + res;
+      }).catch(e=>{
+        console.log(e);
+      })
+}
+}
+
+
 
 
 
