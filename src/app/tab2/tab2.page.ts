ea5d417d4e91348feb0631a4ed2d6b745c8d9ba2
gmartins01 <10GoM@rtins10>
2022-05-30 19:37:45 +0100

    Upload de imagens pela camera

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Camera} from '@ionic-native/camera/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 
 
 @Component({
@@ -7,36 +7,34 @@ import {Camera} from '@ionic-native/camera/ngx';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page  {
+export class Tab2Page {
 
-imgURL;
+  imgURL;
 
-constructor(
-private camera:Camera
-  ) {
-}
-getCamera() {
-  this.camera.getPicture({
-    sourceType:this.camera.PictureSourceType.CAMERA,
-    destinationType:this.camera.DestinationType.FILE_URI,
-  }).then((res)  =>{
+  constructor(private camera: Camera) {
+  }
+  getCamera() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.DATA_URL,
+    }).then((res) => {
 
-this.imgURL = res;
-  }).catch(e=>{
-    console.log(e);
-  })
+      this.imgURL = 'data:image/jpeg;base64,' + res;
+    }).catch(e => {
+      console.log(e);
+    })
 
-}
-getGallery(){
-  this.camera.getPicture({
-    sourceType:this.camera.PictureSourceType.PHOTOLIBRARY,
-    destinationType:this.camera.DestinationType.DATA_URL
-  }).then((res)  =>{
-    this.imgURL = 'data:image/jpeg;base64,' + res;
-      }).catch(e=>{
-        console.log(e);
-      })
-}
+  }
+  getGallery() {
+    this.camera.getPicture({
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then((res) => {
+      this.imgURL = 'data:image/jpeg;base64,' + res;
+    }).catch(e => {
+      console.log(e);
+    })
+  }
 }
 
 
